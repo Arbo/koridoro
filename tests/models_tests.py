562b63448e12eae25961af1c66e33ab7a5e4566b
Bram Marien <bram.marien@gmail.com>
2012-04-02 20:02:20 +0200

    added jog method

--- tests/models_tests.py
+++ tests/models_tests.py
@@ -14,11 +14,39 @@ class MaleAtheleteTests(unittest.TestCase):
 
         self.assertEqual(athlete.energy_level, energy_level)
 
+    @attr('maleathlete')
     def testWalkReducesEnergyLevel(self):
         athlete = models.MaleAthlete(1000)
 
         athlete.walk()
         self.assertEqual(athlete.energy_level, 999)
+
+    @attr('maleathlete')
+    def testWalkWithTimeUnitsReducesEnergyLevel(self):
+        athlete = models.MaleAthlete(1000)
+
+        athlete.walk(time=5)
+        self.assertEqual(athlete.energy_level, 995)
+
+    @attr('maleathlete')
+    def testJogReducesEnergyLevel(self):
+        orig = 1000
+        athlete = models.MaleAthlete(orig)
+
+        athlete.jog()
+        self.assertEqual(athlete.energy_level, orig-models.JOG_ENERGY)
+
+    @attr('maleathlete')
+    def testJogWithTimeUnitsReducesEnergyLevel(self):
+        orig = 1000
+        athlete = models.MaleAthlete(orig)
+
+        athlete.jog(time=5)
+        self.assertEqual(athlete.energy_level, orig-5*models.JOG_ENERGY)
+
+
+
+
 
 
 if __name__ == "__main__":
